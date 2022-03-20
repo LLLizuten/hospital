95b74c1048b0c7ff5495cbfb5b569f3eeb67e813
EOE233 <83290185+EOE233@users.noreply.github.com>
2022-03-20 17:41:37 +0800

    Update PatientAlterServlet.java

--- src/main/controller/PatientAlterServlet.java
+++ src/main/controller/PatientAlterServlet.java
@@ -13,10 +13,26 @@ import java.io.IOException;
  * @create 2022-03-17 10:54
  */
 public class PatientAlterServlet extends HttpServlet {
-    private DBBean dbBean = new DBBean();
+    private DBBean dbBean;
+
+    public PatientAlterServlet() {
+        this.dbBean = new DBBean();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String P_id = req.getParameter("P_id");
+        String P_psd = req.getParameter("P_psd");
+        String P_name = req.getParameter("P_name");
+        String P_sex = req.getParameter("P_sex");
+        String P_age = req.getParameter("P_age");
+
+        String sql = "update patient SET P_psd='" + P_psd + "' where P_id='" + P_id + "'";
+        dbBean.update(sql);
     }
 }
