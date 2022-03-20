fe8d2b503a070e81647595f404efd4a170cab1e6
EOE233 <83290185+EOE233@users.noreply.github.com>
2022-03-20 17:42:13 +0800

    Update PatientDeleteServlet.java

--- src/main/controller/PatientDeleteServlet.java
+++ src/main/controller/PatientDeleteServlet.java
@@ -1,5 +1,7 @@
 package controller;
 
+import bean.DBBean;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +14,24 @@ import java.io.IOException;
  * 患者删除
  */
 public class PatientDeleteServlet extends HttpServlet {
+    private DBBean dbBean;
+
+
+    public PatientDeleteServlet() {
+        this.dbBean = new DBBean();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //获取请求参数
         String P_id = req.getParameter("P_id");
+
+        String sql = "delete from patient where P_id='" + P_id + "'";
+        dbBean.update(sql);
     }
 }
